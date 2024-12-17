3f4e615e9de8a7867bbb2cd84b625a6d1a7d8c58
Mark Lanari <mark@lanari.fi>
2024-12-17 11:20:51 +0000

    Completed day 12 in Julia.

--- Julia/day12.jl
+++ Julia/day12.jl
@@ -8,7 +8,7 @@ using InteractiveUtils
 function getInput()
     input = readlines("day12_input.txt")
 
-    input = split("RRRRIICCFF
+    input2 = split("RRRRIICCFF
 RRRRIICCCF
 VVRRRCCFFF
 VVRCCCJFFF
@@ -19,17 +19,19 @@ MIIIIIJJEE
 MIIISIJEEE
 MMMISSJEEE", "\n")
 
-	input = split("EEEEE
+	input2 = split("EEEEE
 EXXXX
 EEEEE
 EXXXX
 EEEEE", "\n"
 )
 
-	input = split("AAAA
-BBCD
-BBCC
-EEEC", "\n"
+	input2 = split("AAAAAA
+AAABBA
+AAABBA
+ABBAAA
+ABBAAA
+AAAAAA", "\n"
 )
 
     strArray = Array{String}(undef, size(input)[1], length(input[1]))
@@ -43,67 +45,164 @@ EEEC", "\n"
     return strArray
 end
 
-# ╔═╡ ef4a3461-fa7d-4efd-8add-92706b30adce
-function part2()
-	map = getInput()
-
-	println(evalMap2(map))
-end
-
-# ╔═╡ 862e51d7-aa72-425f-a382-cb36eee4390f
-
-
-# ╔═╡ dc3c8a3c-c0a0-4d78-a58d-18509a6bae8b
-function evalBlock(map, visited, i, j)
-	if i<1 || i>size(map)[1] || j<1 || j>size(map)[2] || visited[i,j] == "."
-		return (visited, 0, 0)
+# ╔═╡ 2ed3d290-178b-4f49-acfc-7725e6ecea42
+# same col, but adjacent row
+function existsAdjacentX(what, thisArrayThing)
+	for item in thisArrayThing
+		if item[1] == what[1] + 1 || item[1] == what[1] - 1
+			if item[2] == what[2]
+				return true
+			end
+		end
 	end
 
+	return false
+end
+
+# ╔═╡ c11474a9-807c-42bb-96e0-de76a996c48c
+# same row, but adjacent col
+function existsAdjacentY(what, thisArrayThing)
+	for item in thisArrayThing
+		if item[1] == what[1]
+			if item[2] == what[2] + 1 || item[2] == what[2] - 1
+				return true
+			end
+		end
+	end
+	return false
+end
+
+# ╔═╡ dc3c8a3c-c0a0-4d78-a58d-18509a6bae8b
+function evalBlock(map, visited, row, col, nSides, onLeft, onTop, onRight, onBottom, original)
+
+	if row<1 || row>size(map)[1] || col<1 || col>size(map)[2] || visited[row,col] == "."
+		return (visited, 0, 0, nSides, onLeft, onTop, onRight, onBottom)
+	end
+
+
 	newVisited = deepcopy(visited)
-	newVisited[i,j] = "."
+	newVisited[row,col] = "."
     block=1
     perim=0
 
-    if i==1
+
+    if row==1
         perim += 1
-	elseif map[i-1,j] != map[i,j]
+		push!(onTop, (row,col))
+	elseif map[row-1,col] != map[row,col]
 		perim += 1
+		push!(onTop, (row,col))
 	else
-		(newVisited, newBlock, newPerim) = evalBlock(map, newVisited, i-1, j)
+		(newVisited, newBlock, newPerim, nSides, onLeft, onTop, onRight, onBottom) = evalBlock(map, newVisited, row-1, col, nSides, onLeft, onTop, onRight, onBottom, false)
 		block += newBlock
 		perim += newPerim
 	end
 
-	if i==size(map)[1]
+	if row==size(map)[1]
 		perim += 1
-	elseif map[i+1,j] != map[i,j]
+		push!(onBottom, (row,col))
+	elseif map[row+1,col] != map[row,col]
 		perim += 1
+		push!(onBottom, (row,col))
 	else
-		(newVisited, newBlock, newPerim) = evalBlock(map, newVisited, i+1, j)
+		(newVisited, newBlock, newPerim, nSides, onLeft, onTop, onRight, onBottom) = evalBlock(map, newVisited, row+1, col, nSides, onLeft, onTop, onRight, onBottom, false)
 		block += newBlock
 		perim += newPerim
 	end
 
-	if j==1
+	if col==1
 		perim += 1
-	elseif map[i, j-1] != map[i,j]
+		push!(onLeft, (row,col))
+	elseif map[row, col-1] != map[row,col]
 		perim += 1
+		push!(onLeft, (row,col))
 	else
-		(newVisited, newBlock, newPerim) = evalBlock(map, newVisited, i, j-1)
+		(newVisited, newBlock, newPerim, nSides, onLeft, onTop, onRight, onBottom) = evalBlock(map, newVisited, row, col-1, nSides, onLeft, onTop, onRight, onBottom, false)
 		block += newBlock
 		perim += newPerim
 	end
 
-	if j == size(map)[2]
+	if col == size(map)[2]
 		perim += 1
-	elseif map[i,j+1] != map[i,j]
+		push!(onRight, (row,col))
+	elseif map[row,col+1] != map[row,col]
 		perim += 1
+		push!(onRight, (row,col))
 	else
-		(newVisited, newBlock, newPerim) = evalBlock(map, newVisited, i, j+1)
+		(newVisited, newBlock, newPerim, nSides, onLeft, onTop, onRight, onBottom) = evalBlock(map, newVisited, row, col+1, nSides, onLeft, onTop, onRight, onBottom, false)
 		block += newBlock
 		perim += newPerim
 	end
-	return (newVisited, block, perim)
+
+	if original
+		nSides = 0
+		sort!(onTop, by=x->x[1]x[2])
+		sort!(onBottom, by=x->x[1]x[2])
+		sort!(onLeft, by=x->x[2]x[1])
+		sort!(onRight, by=x->x[2]x[1])
+	end
+	
+	for i=1:size(onTop)[1]
+		if i==size(onTop)[1]
+			nSides += 1
+		elseif existsAdjacentY(onTop[i], onTop[i+1:end])
+			continue
+		else
+			nSides += 1
+		end
+	end
+
+	for i = 1:size(onBottom)[1]
+		if i==size(onBottom)[1]
+			nSides += 1
+		elseif existsAdjacentY(onBottom[i], onBottom[i+1:end])
+			continue
+		else
+			nSides += 1
+		end
+	end
+
+	for i = 1:size(onLeft)[1]
+		if i==size(onLeft)[1]
+			nSides += 1
+		elseif existsAdjacentX(onLeft[i], onLeft[i+1:end])
+			continue
+		else
+			nSides += 1
+		end
+	end
+
+	for i = 1:size(onRight)[1]
+		if i==size(onRight)[1]
+			nSides += 1
+		elseif existsAdjacentX(onRight[i], onRight[i+1:end])
+			continue
+		else
+			nSides += 1
+		end
+	end
+	
+	return (newVisited, block, perim, nSides, onLeft, onTop, onRight, onBottom)
+end
+
+# ╔═╡ 8a6a8916-4a4d-4545-ae98-c2999ebc58c1
+function existsX(what, thisArrayThing)
+	for item in thisArrayThing
+		if item[1] == what
+			return true
+		end
+	end
+	return false
+end
+
+# ╔═╡ f9df03a7-c092-493d-b1e2-2408d6028ccb
+function existsY(what, thisArrayThing)
+	for item in thisArrayThing
+		if item[2] == what
+			return true
+		end
+	end
+	return false
 end
 
 # ╔═╡ c119974f-7fe8-48f0-a1b8-276b314dc450
@@ -116,36 +215,30 @@ function evalMap(map)
 	end
 
 	cost = 0
+	cost2 = 0
 	
 	for i in 1:size(map)[1]
 		for j in 1:size(map)[2]
 			if visited[i,j] != "."
-				(visited, block, perim) = evalBlock(map, visited, i, j)
-				println(map[i,j], ",", block, ",", perim)
+				(visited, block, perim, nSides) = evalBlock(map, visited, i, j, 0, [], [], [], [], true)
 				cost += block*perim
+				cost2 += block*nSides
 			end
 		end
 	end
 
-	return cost
-	
+	return (cost, cost2)
 end
 
 # ╔═╡ 47efdb1c-7f57-4ad4-962a-334816a7fc58
-function part1()
-	map = getInput()
-
-	println(evalMap(map))
-end
-
-# ╔═╡ d6149cbf-3b78-48b4-9cb4-001084d51bd8
-part1()
+println(evalMap(getInput()))
 
 # ╔═╡ Cell order:
-# ╠═d6149cbf-3b78-48b4-9cb4-001084d51bd8
 # ╠═47efdb1c-7f57-4ad4-962a-334816a7fc58
-# ╠═ef4a3461-fa7d-4efd-8add-92706b30adce
 # ╠═804028ac-ba14-11ef-363d-6b4e1cd67750
-# ╠═862e51d7-aa72-425f-a382-cb36eee4390f
 # ╠═dc3c8a3c-c0a0-4d78-a58d-18509a6bae8b
+# ╠═2ed3d290-178b-4f49-acfc-7725e6ecea42
+# ╠═c11474a9-807c-42bb-96e0-de76a996c48c
+# ╠═8a6a8916-4a4d-4545-ae98-c2999ebc58c1
+# ╠═f9df03a7-c092-493d-b1e2-2408d6028ccb
 # ╠═c119974f-7fe8-48f0-a1b8-276b314dc450
